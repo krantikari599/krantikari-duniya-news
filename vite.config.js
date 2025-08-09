8c74ff3e42ed291e0704c461ce52abe03ea51d56
Yogesh Amrute <yogeshamrute12345@gmail.com>
2025-08-09 18:22:22 +0530

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-      tailwindcss(),
+  base: '/krantikari-duniya/', // IMPORTANT: change if repo name changes
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
 })
