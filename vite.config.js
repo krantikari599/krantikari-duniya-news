9d3976702284f60fcd822bdaab385faf7848cdca
krantikari599 <krantikari599@gmail.com>
2025-08-10 06:42:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [git add .
-git commit -m "Initial commit for krantikari-duniya-news"
-
+  plugins: [
     react(),
     tailwindcss(),
   ],
