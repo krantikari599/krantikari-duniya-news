58c91272011895c1909daab0a71e53db2e1991bf
Yogesh Amrute <yogeshamrute12345@gmail.com>
2025-08-10 06:34:44 +0530

    Initial commit for krantikari-duniya-news

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
+  plugins: [git add .
+git commit -m "Initial commit for krantikari-duniya-news"
+
     react(),
     tailwindcss(),
   ],
